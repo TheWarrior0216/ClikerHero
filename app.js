2ed38bf5874346c76b808945852963e22598f349
Seth Baxter <TheWarrior0216@gmail.com>
2024-06-14 12:32:54 -0600

    BackEnd Mostly Complete

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 let darkEnergy = 0
+let clickCollection = 0
+let autoCollection = 0
+
 let clickUpgrades = [
   {
     name: 'pickaxe',
-    price: 100,
+    price: 10,
     quantity: 0,
     multiplier: 1
   }
@@ -18,9 +21,52 @@ let automaticUpgrades = [
 ];
 function Harvest() {
   darkEnergy++
+  clickUpgrades.forEach((item) => clickCollection = (item.quantity *= item.multiplier))
+  darkEnergy += clickCollection
+  Update()
+}
+function autoHarvest() {
+  automaticUpgrades.forEach((item) => autoCollection = (item.multiplier *= item.quantity))
+  console.log(autoCollection)
   Update()
 }
 function Update() {
+  darkEnergy += autoCollection
   const darkEnergyEle = document.getElementById('totalDarkEnergy')
-  darkEnergyEle.innerHTML = `${darkEnergy}`
+  const pickaxeEle = document.getElementById('currentPickaxes')
+  const clickCollectionEle = document.getElementById('currentClick')
+  const autoCollectionEle = document.getElementById('currentAuto')
+
+  darkEnergyEle.innerHTML = `Dark Energy = ${darkEnergy}`
+  pickaxeEle.innerHTML = ` You have ${clickUpgrades[0].quantity} Pickaxes`
+  clickCollectionEle.innerHTML = `${clickCollection}`
+  autoCollectionEle.innerHTML = `${autoCollection}`
 }
+function buyPickaxe(UpgradeType, Clicktype) {
+  if (UpgradeType == 'clickUpgrades') {
+    const upgrade = clickUpgrades
+    const upgradeFound = upgrade.find((upgrade) => upgrade.name == Clicktype)
+
+    if (darkEnergy >= upgradeFound.price) {
+      darkEnergy -= upgradeFound.price
+      upgradeFound.quantity++
+      console.log('purchased')
+    }
+    else (console.log('Not Enough Money'))
+    Update()
+  }
+  if (UpgradeType == 'automaticUpgrades') {
+    const upgrade = automaticUpgrades
+    const upgradeFound = upgrade.find((upgrade) => upgrade.name == Clicktype)
+
+    if (darkEnergy >= upgradeFound.price) {
+      darkEnergy -= upgradeFound.price
+      upgradeFound.quantity++
+      console.log('purchased')
+    }
+    else (console.log('Not Enough Money'))
+    Update()
+  }
+}
+Update()
+setInterval(autoHarvest, 3000)
